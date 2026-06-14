1d12fdca936199a2e125845320768aa6a1179eb2
mizuwonomu <sonn3061@gmail.com>
2026-06-14 06:29:56 +0000

    feat(backend): calling api to invoke both models at once
    - Changed api request to invoke both models for comparision output calling
    - Also modified frontend for cleaner display

--- backend/api.py
+++ backend/api.py
@@ -19,6 +19,9 @@ class PredictRequest(BaseModel):
     text: str
     model: str
 
+class PredictBothRequest(BaseModel):
+    text: str
+
 def run_prediction(text: str, model: str):
     model = model.strip().lower()
 
@@ -33,6 +36,19 @@ def predict_sentiment(request: PredictRequest):
     try:
         result = run_prediction(request.text, request.model)
         return result
+    except Exception as e:
+        return {
+            "error": str(e)
+        }
+
+@app.post("/predict_both")
+def predict_both(request: PredictBothRequest):
+    print(f"Both models, Text: {request.text}")
+    try:
+        return {
+            "ann": predict_ann(request.text),
+            "lstm": predict_lstm(request.text),
+        }
     except Exception as e:
         return {
             "error": str(e)
